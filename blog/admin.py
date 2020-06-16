05823e363fde139fcf3885e04568bf1d3c2d661d
pengpengfly <495215592@qq.com>
2020-06-16 19:12:04 +0800

    My Django Girls app, first commit

---
+++ blog/admin.py
@@ -0,0 +1,6 @@
+
+from django.contrib import admin
+from .models import Post
+
+print('OK')
+admin.site.register(Post)
---
+++ blog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Musician(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=100)
+
+class Album(models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_stars = models.IntegerField()
+
+class Fruit(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
